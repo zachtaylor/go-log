228af07aa5482d4da36ea1ab5514c32770fcb3d6
Zach Taylor <zach@ztaylor.me>
2019-04-10 10:45:03 -0500

    add GetLevel

--- level.go
+++ level.go
@@ -1,5 +1,7 @@
 package log
 
+import "errors"
+
 // Level is used to rank the importance of logs
 type Level uint8
 
@@ -15,3 +17,21 @@ const (
 	// LevelTrace is a sentinal value that is guaranteed to print
 	LevelTrace
 )
+
+// GetLevel returns the level named, if valid
+func GetLevel(level string) (Level, error) {
+	switch level {
+	case "debug":
+		return LevelDebug, nil
+	case "info":
+		return LevelInfo, nil
+	case "warn":
+		return LevelWarn, nil
+	case "error":
+		return LevelError, nil
+	case "trace":
+		return LevelTrace, nil
+	default:
+		return LevelDebug, errors.New("log level unknown: " + level)
+	}
+}
